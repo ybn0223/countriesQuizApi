147a0262f669f6a614095e4bf7089bc801740f69
Yassine B <yassinebenmendour2@gmail.com>
2024-12-15 22:02:53 +0100

    added route

--- routers/api.ts
+++ routers/api.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import GET_COUNTRIES from './getCountries';
 import PROFILE_PICTURE from './profilePicture';
 import CREATE_USER from './createUser';
+import LOGIN_USER from './loginUser';
 
 const APIROUTER = express.Router();
 	
 APIROUTER.use('/', GET_COUNTRIES);
 APIROUTER.use('/', PROFILE_PICTURE);
 APIROUTER.use('/', CREATE_USER);
+APIROUTER.use('/', LOGIN_USER);
 
 
 export default APIROUTER;
